8fcb6b0b5f978743557be3deb3fcb2c71265f7e6
Your Name <you@example.com>
2020-08-10 11:50:03 +0800

    可开发

---
+++ src/views/things/Contents.js
@@ -0,0 +1,99 @@
+export const Contents = [
+    [{
+        src: require('@/assets/images/Things/she1.png'),
+        asrc: require('@/assets/images/Things/she2.png'),
+        name: '移动社交',
+        width: '36px',
+        height: '35px',
+        id: 1
+    }, {
+        src: require('@/assets/images/Things/zhi1.png'),
+        asrc: require('@/assets/images/Things/zhi2.png'),
+        name: '视频直播',
+        width: '40px',
+        height: '34px',
+        id: 2
+    }, {
+        src: require('@/assets/images/Things/jiao1.png'),
+        asrc: require('@/assets/images/Things/jiao2.png'),
+        name: '线上教育',
+        width: '44px',
+        height: '27px',
+        id: 3
+    }, {
+        src: require('@/assets/images/Things/yi1.png'),
+        asrc: require('@/assets/images/Things/yi2.png'),
+        name: '智慧医疗',
+        width: '34px',
+        height: '30px',
+        id: 4
+    }, {
+        src: require('@/assets/images/Things/jian1.png'),
+        asrc: require('@/assets/images/Things/jian2.png'),
+        name: '智慧健身',
+        width: '38px',
+        height: '26px',
+        id: 5
+    }],
+    [{
+        src: require('@/assets/images/Things/shang1.png'),
+        asrc: require('@/assets/images/Things/shang2.png'),
+        name: '移动商城',
+        width: '38px',
+        height: '35px',
+        id: 6
+    }, {
+        src: require('@/assets/images/Things/wai1.png'),
+        asrc: require('@/assets/images/Things/wai2.png'),
+        name: '智慧外卖',
+        width: '40px',
+        height: '34px',
+        id: 7
+    }, {
+        src: require('@/assets/images/Things/pao1.png'),
+        asrc: require('@/assets/images/Things/pao2.png'),
+        name: '跑腿外送',
+        width: '36px',
+        height: '36px',
+        id: 8
+    }, {
+        src: require('@/assets/images/Things/jiaoyou1.png'),
+        asrc: require('@/assets/images/Things/jiaoyou2.png'),
+        name: '移动交友',
+        width: '36px',
+        height: '28px',
+        id: 9
+    }, {
+        src: require('@/assets/images/Things/qi1.png'),
+        asrc: require('@/assets/images/Things/qi2.png'),
+        name: '其他行业',
+        width: '38px',
+        height: '12px',
+        id: 10
+    }]
+
+]
+
+
+export const system = [{
+    name: '行业数据分析',
+    id: '01'
+}, {
+    name: '品牌定位规范',
+    id: '02'
+}, {
+    name: '功能需求调研',
+    id: '03'
+}, {
+    name: '交互视觉设计',
+    id: '04'
+}, {
+    name: '双端同步开发',
+    id: '05'
+}, {
+    name: '后端并行开发',
+    id: '06'
+}, {
+    name: '测试上线交付使用',
+    id: '07'
+},]
